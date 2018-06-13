03aebcd1194cb911d017e03b74a8fb8d2c77abca
Mohamed Yehia <massem1983@yahoo.com>
2018-06-13 17:17:55 -0400

    FizzWriter.java

--- 16-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
+++ 16-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
@@ -8,42 +8,46 @@ import java.util.Scanner;
 public class WordSearch {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		
 
-		System.out.println("Please enter the word that you want to search: ");
-		String word = input.nextLine();
-		System.out.println("Does it has to be case sensitive? y/n: ");
-		String isSensitive = input.nextLine();
-		int counterLine = 0;
+			Scanner input = new Scanner(System.in);
 
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("test_quiz.txt"));
-	
-			while (true) {
-				String line = reader.readLine();
+			System.out.println("Please enter the word that you want to search: ");
+			String word = input.nextLine();
+			System.out.println("Does it has to be case sensitive? y/n: ");
+			String isSensitive = input.nextLine();
+			int counterLine = 0;
 
-				if (line == null) {
-					break;
-					}
-				
-				if (isSensitive.equals("y")) {
-					if(line.contains(word)) {
-						System.out.println(counterLine + ") " + line );
-					}
-				}
-				else { // is not sensitive
-					if(line.toLowerCase().contains(word.toLowerCase())) {
-						System.out.println(counterLine + ") " + line);
-					}
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader("test_quiz.txt"));
+		
+				while (true) {
+					String line = reader.readLine();
+
+					if (line == null) {
+						break;
+						}
 					
+					if (isSensitive.equals("y")) {
+						if(line.contains(word)) {
+							System.out.println(counterLine + ") " + line );
+						}
+					}
+					else { // is not sensitive
+						if(line.toLowerCase().contains(word.toLowerCase())) {
+							System.out.println(counterLine + ") " + line);
+						}
+						
+					}
+					counterLine++;
 				}
-				counterLine++;
+				reader.close();
+
+			} catch (Exception e) {
+
 			}
-			reader.close();
 
-		} catch (Exception e) {
 
-		}
 
 	}
 
--- 17-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
+++ 17-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
@@ -1,10 +1,36 @@
 package com.techelevator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		PrintWriter printtoFile = new PrintWriter("FizzBuzz.txt");
+			for (int i = 1; i <301; i++) 
+			{
+				if (i % 3 == 0 && i % 5 == 0) {
+					
+		            printtoFile.println("FizzBuzz");	
+			  
+				} else if (i % 3 == 0 ||Integer.toString(i).equals("3")) {
+					
+					printtoFile.println("Fizz");	
 		
 
-	}
+			    } else if (i % 5 == 0 || Integer.toString(i).equals("5")) {
+				    
+			    		printtoFile.println("Buzz");	
 
+			    }else
+			    
+			    		printtoFile.println(i);
+			}
+			printtoFile.close();
+	}	  
 }
